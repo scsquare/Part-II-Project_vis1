0c775c0276dad67c5d5623fd7f71d9ad17fdc10d
stephaniecho <stephanie.cho33@gmail.com>
2024-01-17 09:53:05 +0000

    Fixed axis label

--- js/myplot.js
+++ js/myplot.js
@@ -188,14 +188,14 @@ function mysecondplot(){
         "translate(" + margin.left + "," + margin.top + ")");
 
     //axis labels
-    svg.append("text").attr("transform", "translate(" + (width / 2) + " ," + (height + margin.bottom+10) + ")").text("Tempo (bpm)");
+    svg.append("text").attr("transform", "translate(" + (width / 2) + " ," + (height + margin.bottom+10) + ")").text("Tempo");
     svg.append("text")
         .attr("transform", "rotate(-90)")
         .attr("y", 0 - margin.left)
         .attr("x",0 - (height / 2))
         .attr("dy", "1em")
         .style("text-anchor", "middle")
-        .text("Loudness (dB)");
+        .text("Loudness");
 
 
 
